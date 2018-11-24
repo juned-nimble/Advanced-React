cba8e5d8db5f1ce8bb5ed0714fe7553e3f98c48a
juned-nimble <juned@nimblechapps.com>
2018-11-24 10:18:42 +0530

    Module 8

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -22,10 +22,43 @@ const Query = {
   },
   async users(parent, args, ctx, info) {
     if (!ctx.request.userId) {
-      throw new Error("You must be logged in!");
+      throw new Error("Please login!");
     }
     hasPermission(ctx.request.user, ["ADMIN", "PERMISSIONUPDATE"]);
     return ctx.db.query.users({}, info);
+  },
+  async order(parent, args, ctx, info) {
+    if (!ctx.request.userId) {
+      throw new Error("Please login!");
+    }
+    const order = await ctx.db.query.order(
+      {
+        where: { id: args.id }
+      },
+      info
+    );
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes(
+      "ADMIN"
+    );
+    if (!ownsOrder || !hasPermission) {
+      throw new Error("You don't have permission to see this order!");
+    }
+    return order;
+  },
+  async orders(parent, args, ctx, info) {
+    let userId = ctx.request.userId;
+    if (!userId) {
+      throw new Error("Please login!");
+    }
+    return ctx.db.query.orders(
+      {
+        where: {
+          user: { id: userId }
+        }
+      },
+      info
+    );
   }
   // async items(parent, args, ctx, info) {
   //   console.log('Getting Items!!');
